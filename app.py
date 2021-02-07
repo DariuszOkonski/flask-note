284bc6277c5278c285e0ba950d48b0659847a2d0
Dariusz Okonski <darek200180@gmail.com>
2021-02-07 17:39:55 +0000

    add index.html template

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!!!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/note')
+def note():
+    return '<h1>my notes!!!</h1>'
 
 
 if __name__ == '__main__':
