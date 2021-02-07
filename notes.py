279cdd46e11cca3969d6d043ca40c37553b91068
Dariusz Okonski <darek200180@gmail.com>
2021-02-07 18:56:38 +0000

    add funcionality to write and read from file

--- notes.py
+++ notes.py
@@ -1,7 +1,9 @@
-notes = ["Pierwsza notatka", "Druga notatka", "Trzecia notatka"]
+NOTES_FILE = 'notes.txt'
 
 def get_notes():
-    return notes
+    with open(NOTES_FILE, "r") as file:
+        return file.readlines()
 
 def add_note_to_list(note):
-    notes.append(note)
+    with open(NOTES_FILE, "a") as file:
+        file.write(note + "\n")
