273942b178627d4277526cea747350aafaa50db5
Dariusz Okonski <darek200180@gmail.com>
2021-02-07 18:42:12 +0000

    add funcionality to add note

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 from user_data import get_name
-from notes import get_notes
+from notes import get_notes, add_note_to_list
 
 app = Flask(__name__)
 
@@ -18,9 +18,19 @@ def list_notes():
     return render_template('notes.html',
                            notes=notes)
 
+
 @app.route('/add_note')
-def add_note():
+def display_add_note():
     return render_template('add_note.html')
+
+
+@app.route('/add_note', methods=['POST'])
+def add_note():
+    note_dict = dict(request.form)
+
+    add_note_to_list(note_dict['note'])
+
+    return redirect(url_for('list_notes'))
 
 
 if __name__ == '__main__':
--- notes.py
+++ notes.py
@@ -2,3 +2,6 @@ notes = ["Pierwsza notatka", "Druga notatka", "Trzecia notatka"]
 
 def get_notes():
     return notes
+
+def add_note_to_list(note):
+    notes.append(note)
